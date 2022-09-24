81ba51e9cc2b6f93a3ef8dcb2421e3888c4de8d7
mulukimwangeka <mwangekaruth@gmail.com>
2022-09-24 22:59:32 +0300

    Test are running

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id= "about">
+    <h2>About me</h2>
+    <p>I am an ambitious girl with so many dreams to achieve</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
+  
 }
 
 export default About;
